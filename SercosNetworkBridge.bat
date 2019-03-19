0653ff6a604ba1e602c2e10b2f89326546a00916
Keaton Holappa <keaton.holappa@gmail.com>
2019-03-19 11:09:31 -0400

    Added updated file
    Updated the file to check for admin mode immediately, and allows for user to change default subnets via the tool.

--- SercosNetworkBridge.bat
+++ SercosNetworkBridge.bat
@@ -1,21 +1,14 @@
 @echo off
-Goto :endComment
+Goto :endComment	
 
-	***********************************************************************************
-	    ____  ____  _____ ________  __       ____  _______  __ ____  ____  ________  __
-	   / __ )/ __ \/ ___// ____/ / / /      / __ \/ ____/ |/ // __ \/ __ \/_  __/ / / /
-	  / __  / / / /\__ \/ /   / /_/ /      / /_/ / __/  |   // /_/ / / / / / / / /_/ / 
-	 / /_/ / /_/ /___/ / /___/ __  /      / _, _/ /___ /   |/ _, _/ /_/ / / / / __  /  
-	/_____/\____//____/\____/_/ /_/      /_/ |_/_____//_/|_/_/ |_|\____/ /_/ /_/ /_/   
-
-	***********************************************************************************
+	***************************************************************************************************************************
 
 	Bosch Rexroth Corporation
 	2315 City Line Rd. Bethlehem, PA 18017
 	Author: Keaton Holappa
-	Date: 7/11/2018
+	Date: 3/14/2019
 	   
-	ATTENTION! This script may need to be run as an administrator to operate correctly!
+	ATTENTION! This script needs to be run as an administrator to run!
 	   
 	This batch file allows a user to connect to a device (or devices) on an isolated Sercos III
 	network through an MLC's (IndraControl L or XM) internal routing. I.e. it is possible to 
@@ -26,72 +19,90 @@ Goto :endComment
 
 	This script uses the subnets of the Sercos and MLC (Engineering port) networks,
 	as well as the IP address of the MLC. All of these values must be known.
-	***********************************************************************
+	***************************************************************************************************************************
 
 :endComment
 
-   rem | Hardcode the subnet masks for the Sercos and MLC Engineering networks. These can only be changed by editing this file!
-   set sercosSubnet=172.31.0.0
-   set mlcSubnet=255.255.0.0
-
-   rem | create the two possible commands, temporarily or permanently add the route
-   set tempAdd=route add %sercosSubnet% mask %mlcSubnet%
-   set permAdd=route -p add %sercosSubnet% mask %mlcSubnet%
+   rem | Set the default subnet masks for the Sercos and MLC Engineering networks.
+   set defSercosSubnet=172.31.0.0
+   set defMlcSubnet=255.255.0.0
    
    rem | *********************************************************
    rem | This begins the user interaction with the scipt
    echo This script allows you to connect to a Sercos III network via an MLC Engineering Network.
-   rem | print to the user the hardcoded values of the Sercos and MLC Engineering Subnets
-   echo Sercos Subnet is %sercosSubnet%and MLC Subnet is %mlcSubnet%. To change these, you must edit this batch file.
-   echo This script may need to be run in administrator mode.
+   rem | print to the user the default values of the Sercos and MLC Engineering Subnets
+   echo The default Sercos Subnet is %defSercosSubnet% and MLC Subnet is %defMlcSubnet%.
    
    :ScriptStart
+   rem | Check if the user has run the program in Admin mode
+   net session >nul 2>&1
+   IF NOT %errorLevel% == 0 (
+		echo ********************************************************
+		echo You must run this script in Administrator mode
+		Goto :ScriptEnd
+	)
+
    echo ********************************************************
-   choice /m "Do you want to permenantly add this route to the settings of the PC? "
-   IF "%errorlevel%" == "2" (
-	set commandString=%tempAdd%
-	Goto :Continue
-   )
+   choice /m "Do you want to use the default Sercos III subnet settings?"
+   IF %errorlevel% == 2 (
+   
+		setlocal enabledelayedexpansion
+	
+		:SetSubnets
+		rem | re-write the default subnet values
+		set /p sercosSubnet="Enter the Subnet for your Sercos III Network: "
+		call :validateSubnet !sercosSubnet! ret
+		call :validateSubnet !sercosSubnet! is valid || echo Input "!sercosSubnet!" was entered in an invalid format. Please enter a value in the IPv4 form XXX.xxx.XXX.xxx
+		rem | If ret is not 0 the Sercos Subnet is invalid. User must re-enter.
+		IF NOT "!ret!" == "0" (
+			Goto :SetSubnets
+		)
 
-   choice /m "This is not recommended for non-experts. Are you sure you want to permanently add this route?"
-   IF "%errorlevel%" == "1" (
-	set commandString=%permAdd%
-   ) ELSE (
-	set commandString=%tempAdd%
-   )
+		set /p mlcSubnet="Enter Subnet for MLC Network: "
+		call :validateSubnet !mlcSubnet! ret
+		call :validateSubnet !mlcSubnet! is valid || echo Input %mlcSubnet% was entered in an invalid format. Please enter a value in the IPv4 form XXX.xxx.XXX.xxx
 
-   rem | ask the user to enter the IP address of the MLC
+		rem | If ret is not 0 the MLC Subnet is invalid. User must re-enter.
+		IF NOT "!ret!" == "0" (
+			Goto :SetSubnets
+		) 
+	) ELSE (
+		set sercosSubnet=%defSercosSubnet%
+		set mlcSubnet=%defMlcSubnet%
+	)
+
+   rem | First, add the route command
+   set commandString=route add %sercosSubnet% mask %mlcSubnet%
+
    :Continue
+   rem | ask the user to enter the IP address of the MLC
    echo ********************************************************
    set /p ip="Enter IP Address of MLC: "
 
-  set fullCommandString=%commandString% %ip%
-
    rem *********************************************************
-   rem Determine if the entered IP address is a valid IP
-   call :validateIP %ip% ret 
-   rem echo %ip% : return value : !ret!
-
-   rem call with or without variable to get errorlevel
+   rem | Determine if the entered IP address is a valid IP
+   call :validateIP %ip% ret
    call :validateIP %ip% is valid || echo Input "%ip%" is an invalid IP Address. Please enter a value in the IPv4 form XXX.xxx.XXX.xxx
-   
-   rem If ret is 1 the IP is invalid. User must re-enter an IP.
-   IF "%ret%" == "1" (
-		Goto :Continue
+
+   rem | If ret is not 0 the IP is invalid or not entered correctly. User must re-enter an IP.
+   IF NOT "%ret%" == "0" (
+	Goto :Continue
    )
    rem *********************************************************
 
    rem | run the routing command, and formulate an output for the user
+   set fullCommandString=%commandString% %ip%
    for /f "delims=" %%i in ('%fullCommandString%') do set output=%%i
 
    
    IF "%output%"=="The requested operation requires elevation." (
-		echo You must run this script in administrator mode
+	echo You must run this script in administrator mode
    )
    IF "%output%"==" OK!" (
-		echo Success! It is now possible to connect to the %sercosSubnet% subnet through the MLC at %ip%.
+	echo Success! It is now possible to connect to the %sercosSubnet% subnet through the MLC at %ip%.
    )
-	
+
+   :ScriptEnd
    echo Please press any key to terminate the script.
    pause >nul   
    
@@ -99,20 +110,41 @@ Goto :endComment
    rem FUNCTIONS
    rem *************************************************************************   
    :validateIP ipAddress [returnVariable]
-    rem prepare environment
-    setlocal 
+   rem | Prepare environment
+   setlocal 
+	
+   rem | Assume failure in tests : 0=pass 1=fail : same for errorlevel
+   set "_return=1"
 
-    rem asume failure in tests : 0=pass 1=fail : same for errorlevel
-    set "_return=1"
+   rem | Test if address conforms to ip address structure
+   echo %~1^| findstr /b /e /r "[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*" >nul
 
-    rem test if address conforms to ip address structure
-    echo %~1^| findstr /b /e /r "[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*" >nul
-
-    rem if it conforms to structure, test each octet for rage values
-    if not errorlevel 1 for /f "tokens=1-4 delims=." %%a in ("%~1") do (
+   rem | If it conforms to structure, test each octet for rage values
+   if not errorlevel 1 for /f "tokens=1-4 delims=." %%a in ("%~1") do (
         if %%a gtr 0 if %%a lss 255 if %%b leq 255 if %%c leq 255 if %%d gtr 0 if %%d leq 254 set "_return=0"
-    )
+   )
+   
+   rem | Clean and return data/errorlevel to caller
+   endlocal & ( if not "%~2"=="" set "%~2=%_return%" ) & exit /b %_return%
+   
+   :endValidateIP
 
-	:endValidateIP
-    rem clean and return data/errorlevel to caller
-    endlocal & ( if not "%~2"=="" set "%~2=%_return%" ) & exit /b %_return%
+   :validateSubnet subnetMask [returnVariable]
+   rem | Prepare environment
+   setlocal 
+
+   rem | Assume failure in tests : 0=pass 1=fail : same for errorlevel
+   set "_return=1"
+
+   rem | Test if address conforms to ip address structure
+   echo %~1^| findstr /b /e /r "[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*" >nul
+
+   rem | If it conforms to structure, test each octet for rage values
+   if not errorlevel 1 for /f "tokens=1-4 delims=." %%a in ("%~1") do (
+        if %%a geq 0 if %%a leq 255 if %%b geq 0 if %%b leq 255 if %%c geq 0 if %%c leq 255 if %%d geq 0 if %%d leq 255 set "_return=0"
+   )
+   
+   rem | Clean and return data/errorlevel to caller
+   endlocal & ( if not "%~2"=="" set "%~2=%_return%" ) & exit /b %_return%
+
+   :endValidateSubnet
